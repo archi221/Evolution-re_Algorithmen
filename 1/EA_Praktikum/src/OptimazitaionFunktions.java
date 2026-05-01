2128beb8b609faa5096526aa7c66cacce8616c6e
archi221 <wzt511@haw-hamburg.de>
2026-05-01 16:11:30 +0200

    added acley

--- 1/EA_Praktikum/src/OptimazitaionFunktions.java
+++ 1/EA_Praktikum/src/OptimazitaionFunktions.java
@@ -9,4 +9,15 @@ public class OptimazitaionFunktions {
         }
         return summe;
     }
+    public double AcleyFunktion(double[] vector){
+        double firstSum = 0;
+        double secondSum = 0;
+        for (double v : vector) {
+           firstSum += (Math.pow(v, 2) / vector.length);
+           secondSum += (Math.cos(2 * Math.PI * v) / vector.length);
+        }
+        double firstExp = Math.exp(- 0.2 * firstSum);
+        double secondExp = Math.exp(secondSum);
+        return 20 + Math.E - (20 * firstExp) - secondExp;
+    }
 }
